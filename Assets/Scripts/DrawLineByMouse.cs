c08797debb858497c7227333eb28c040cfa325bb
Marina Lis <94225938+MVLis@users.noreply.github.com>
2023-05-06 21:26:34 +0300

    initial commit

---
+++ Assets/Scripts/DrawLineByMouse.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLineByMouse : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(currentPosition);
+            _lineRenderer.positionCount++;
+            _lineRenderer.SetPosition(_lineRenderer.positionCount-1,currentPosition);
+        }
+     
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _lineRenderer.positionCount = 0;
+        }
+    }
+}
