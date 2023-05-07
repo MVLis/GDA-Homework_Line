4d127022ecf77f120fa62e46a5c2173eeb0de7ea
Marina Lis <94225938+MVLis@users.noreply.github.com>
2023-05-07 09:33:44 +0300

    fix

--- Assets/Scripts/DrawLineByMouse.cs
+++ Assets/Scripts/DrawLineByMouse.cs
@@ -6,28 +6,26 @@ using UnityEngine;
 public class DrawLineByMouse : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
-    
-    
+
     private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
-        _lineRenderer.positionCount = 0;
     }
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lineRenderer.positionCount = 0;
+        }
+        
         if (Input.GetMouseButton(0))
         {
             Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //currentPosition.z = 0;
-            Debug.Log(currentPosition);
             _lineRenderer.positionCount++;
             _lineRenderer.SetPosition(_lineRenderer.positionCount-1,currentPosition);
         }
      
-        else if (Input.GetMouseButtonUp(0))
-        {
-            _lineRenderer.positionCount = 0;
-        }
+        
     }
 }
