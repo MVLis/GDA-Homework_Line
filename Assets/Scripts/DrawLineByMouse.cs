1e3400abbda847b1681b0afc15d0db239f765fd6
Marina Lis <94225938+MVLis@users.noreply.github.com>
2023-05-10 14:32:55 +0300

    fix: problems with mousePosition

--- Assets/Scripts/DrawLineByMouse.cs
+++ Assets/Scripts/DrawLineByMouse.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DrawLineByMouse : MonoBehaviour
 {
+    [SerializeField] private float _deep = 10;
+    
     private LineRenderer _lineRenderer;
+    private Camera _camera;
 
     private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
+        _camera=Camera.main;
     }
 
     private void Update()
@@ -21,9 +26,10 @@ public class DrawLineByMouse : MonoBehaviour
         
         if (Input.GetMouseButton(0))
         {
-            Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var currentPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _deep);
+            var drawingPoint = _camera.ScreenToWorldPoint(currentPosition);
             _lineRenderer.positionCount++;
-            _lineRenderer.SetPosition(_lineRenderer.positionCount-1,currentPosition);
+            _lineRenderer.SetPosition(_lineRenderer.positionCount-1,drawingPoint);
         }
      
         
